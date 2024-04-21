3cdf05e5243ca4eafd6a497273ccfb87fbdac857
NaitikKaythwal <naitikkaythwal@gmail.com>
2024-04-21 00:35:09 -0500

    Implemented Product model, controller, views, and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+Product.create(name: "iPhone 13", price: 10.99, description: "The latest iPhone")
+Product.create(name: "Samsung Galaxy S22", price: 20.49, description: "Android smartphone with a stunning display.")
+Product.create(name: "Sony PlayStation 5", price: 15.75, description: "Gaming console for immersive gaming experiences. ")
